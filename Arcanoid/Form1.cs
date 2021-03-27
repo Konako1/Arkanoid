ff0fdfa4707f4f4ea4142a3e7e85806a9d34a98b
Quildy <idisk2017@yandex.ru>
2021-03-27 09:49:14 +0500

    upg

--- Arcanoid/Form1.cs
+++ Arcanoid/Form1.cs
@@ -16,5 +16,10 @@ namespace Arcanoid
     {
       InitializeComponent();
     }
-  }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "barbara";
+        }
+    }
 }
