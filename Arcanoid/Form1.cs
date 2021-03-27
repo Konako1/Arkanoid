fe1c3b71dd35280797f91dee53d3f624893bca8b
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-03-27 09:52:42 +0500

    change form name

--- Arcanoid/Form1.cs
+++ Arcanoid/Form1.cs
@@ -19,7 +19,7 @@ namespace Arcanoid
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            this.Text = "barbara";
+            this.Text = "test";
         }
     }
 }
