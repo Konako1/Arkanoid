a1f4f3425ef14827074cdfbe5dbb7cef0d725ec3
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-05 21:17:48 +0500

    Create Tile.cs

---
+++ Arkanoid/Tile.cs
@@ -0,0 +1,12 @@
+namespace Arcanoid
+{
+    public class Tile
+    {
+        private const int Width = 20;
+        private const int Height = 50;
+        public Tile()
+        {
+            
+        }
+    }
+}
