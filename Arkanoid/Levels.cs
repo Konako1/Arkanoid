30faab1730aef7e45542a8fc811258efcaf33a58
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-05 20:11:07 +0500

    Create Level system

---
+++ Arkanoid/Levels.cs
@@ -0,0 +1,11 @@
+namespace Arcanoid
+{
+    public class Levels
+    {
+        private readonly int Level;
+        public Levels(int level)
+        {
+            Level = level;
+        }
+    }
+}
