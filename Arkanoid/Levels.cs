47d590750180fd6e122632447923bc922ebfef37
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-05 21:18:13 +0500

    Update Levels.cs

--- Arkanoid/Levels.cs
+++ Arkanoid/Levels.cs
@@ -1,11 +1,31 @@
+using System;
+
 namespace Arcanoid
 {
     public class Levels
     {
-        private readonly int Level;
-        public Levels(int level)
+        private string[,] LevelStruct;
+        public Levels (int level)
         {
-            Level = level;
+            LevelBuilder(level);
+        }
+
+        public string[,] Level
+        {
+            get => LevelStruct;
+        }
+
+        private void LevelBuilder(int level)
+        {
+            if (level == 0)
+            {
+                LevelStruct = new string[5,2];
+                for (int i = 0; i < LevelStruct.GetLength(0); i++)
+                for (int j = 0; j < LevelStruct.GetLength(1); j++)
+                {
+                    LevelStruct[i, j] = "*";
+                }
+            }
         }
     }
 }
