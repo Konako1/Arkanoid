08650383b8690b0d4d5823fba77e6eb82f572b3e
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-05 20:10:47 +0500

    Create Ball.cs

---
+++ Arkanoid/Ball.cs
@@ -0,0 +1,26 @@
+namespace Arcanoid
+{
+    public class Ball
+    {
+        private int _speed;
+        const int size = 20;
+
+        public int Speed
+        {
+            get => _speed;
+            private set
+            {
+                if (value == 0) _speed = 1; // пока выбраны условные значения, возможно изменятся в будущем
+                if (value == 1) _speed = 2;
+                if (value == 2) _speed = 3;
+            }
+        }
+
+        public int Size => size;
+
+        public Ball(int difficulty)
+        {
+            Speed = difficulty;
+        }
+    }
+}
