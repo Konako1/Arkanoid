853740fcd8a3ba4eb95260a2ed21bb2b8e33718a
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-05 20:45:20 +0500

    Create Platform.cs

---
+++ Arkanoid/Platform.cs
@@ -0,0 +1,16 @@
+﻿using Arcanoid.Properties;
+
+namespace Arcanoid
+{
+    public class Platform
+    {
+        private const int Width = 20;
+        private readonly int Height;
+
+        public Platform (int difficulty)
+        {
+            if (difficulty == 0) Height = 200; //на каждую сложность своя длинна
+            if (difficulty == 1) Height = 150;
+        }
+    }
+}
