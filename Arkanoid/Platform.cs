52db98fcd02a0d182108be9fdb8308b3bc2dd4d2
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-22 21:45:07 +0500

    Add platform size getter

--- Arkanoid/Platform.cs
+++ Arkanoid/Platform.cs
@@ -6,11 +6,15 @@ namespace rndomNamespace
     {
         private const int Width = 20;
         private readonly int Height;
+        private static int Size;
 
         public Platform (int difficulty)
         {
+            Size = 1;
             if (difficulty == 0) Height = 200; //на каждую сложность своя длинна
             if (difficulty == 1) Height = 150;
         }
+
+        public int GetSize => Size;
     }
 }
