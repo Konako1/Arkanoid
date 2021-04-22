2cc4205cac21c0665b0fb945fac5269f83f408af
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-04-23 00:52:09 +0500

    Update Game form
    -Platform now can move
    -Add ball on form
    -Set window, platform and ball size and position by code

--- Arkanoid/GameScreen.cs
+++ Arkanoid/GameScreen.cs
@@ -16,7 +16,8 @@ namespace rndomNamespace
         public readonly int Level;
         public readonly int Speed;
         public readonly int BallSize;
-        public readonly int PlatformSize;
+        public readonly int PlatformHeight;
+        public readonly int PlatformWidth;
         public GameModel(int difficulty, int level)
         {
             Level = level;
@@ -26,12 +27,47 @@ namespace rndomNamespace
             
             Speed = ball.Speed;
             BallSize = ball.Size;
-            PlatformSize = platform.GetSize; 
+            
+            PlatformHeight = platform.GetHeight;
+            PlatformWidth = platform.GetWidth;
         }
     }
 
     public partial class Arkanoid : Form
     {
+        private readonly int _platformWidth;
+        private readonly int _windowHeight;
+        private readonly int _windowWidth;
+
+        public Arkanoid(GameModel gameModel)
+        {
+            InitializeComponent();
+            
+            _windowHeight = 800; // планируется добавить разный размер окон, а мб и нет
+            _windowWidth = 800;
+            int ballSize = gameModel.BallSize;
+            int platformWidth = gameModel.PlatformWidth;
+            int platformHeight = gameModel.PlatformHeight;
+            
+            StartPosition = FormStartPosition.CenterScreen;
+            
+            Size = new Size(_windowWidth, _windowHeight);
+            MinimumSize = new Size(_windowWidth, _windowHeight);
+            MaximumSize = new Size(_windowWidth, _windowHeight);
+
+            ball.Image = Properties.Resources.ball;
+            ball.Size = new Size(ballSize, ballSize);
+            ball.Location = new Point(_windowWidth / 2 - ballSize / 2, 700 - ballSize);
+            
+            if (platformWidth == 131) platform1.Image = Properties.Resources.platform1;
+            platform1.Size = new Size(platformWidth, platformHeight);
+            platform1.Location = new Point(_windowWidth / 2 - platformWidth / 2, 700);
+            _platformWidth = platformWidth;
+            
+            GameModel game = gameModel;
+            KeyDown += Form1_KeyDown;
+        }
+        
         private void Form1_KeyDown(object obj, KeyEventArgs e)
         {
             int currentKey = e.KeyValue;
@@ -39,24 +75,16 @@ namespace rndomNamespace
             switch (currentKey)
             {
                 case 37:
-                    if (platform1.Left < 0) return;
+                    if (platform1.Left < 3) return;
                     
-                    platform1.Location = new Point(platform1.Location.X - 5, platform1.Location.Y);
+                    platform1.Location = new Point(platform1.Location.X - 10, platform1.Location.Y);
                     break;
                 case 39:
-                    if (platform1.Right > 700) return;
+                    if (platform1.Right > _windowWidth - 19) return;
                     
-                    platform1.Location = new Point(platform1.Location.X + 5, platform1.Location.Y);
+                    platform1.Location = new Point(platform1.Location.X + 10, platform1.Location.Y);
                     break;
             }
-        }
-        
-        public Arkanoid(GameModel gameModel)
-        {
-            InitializeComponent();
-            if (gameModel.PlatformSize == 1) platform1.Image = Properties.Resources.platform1;
-            GameModel game = gameModel;
-            KeyDown += Form1_KeyDown;
         }
     }
 }
