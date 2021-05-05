b2151337d832d8a451418d03e7f9260ff868d1be
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-05-05 12:36:37 +0500

    Add more lvl and update lvl builder

--- Arkanoid/Levels.cs
+++ Arkanoid/Levels.cs
@@ -4,27 +4,35 @@ namespace rndomNamespace
 {
     public class Levels
     {
-        private string[,] LevelStruct;
+        private string[,] _levelStruct;
         public Levels (int level)
         {
             LevelBuilder(level);
         }
 
-        public string[,] Level
+        public string[,] Level => _levelStruct;
+
+        private void StringBuilder(string[,] levelStruct)
         {
-            get => LevelStruct;
+            for (int i = 0; i < levelStruct.GetLength(0); i++)
+            for (int j = 0; j < levelStruct.GetLength(1); j++)
+            {
+                _levelStruct[i, j] = "*";
+            }
         }
 
         private void LevelBuilder(int level)
         {
             if (level == 0)
             {
-                LevelStruct = new string[5,2];
-                for (int i = 0; i < LevelStruct.GetLength(0); i++)
-                for (int j = 0; j < LevelStruct.GetLength(1); j++)
-                {
-                    LevelStruct[i, j] = "*";
-                }
+                _levelStruct = new string[5,2];
+                StringBuilder(_levelStruct);
+            }
+
+            if (level == 1)
+            {
+                _levelStruct = new string[8, 3];
+                StringBuilder(_levelStruct);
             }
         }
     }
