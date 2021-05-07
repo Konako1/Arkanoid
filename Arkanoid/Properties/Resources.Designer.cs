adb16f2cc44855f9b4308e1088af9765e61ea1de
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-05-07 12:17:10 +0500

    Add bg

--- Arkanoid/Properties/Resources.Designer.cs
+++ Arkanoid/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace rndomNamespace.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap bg {
+            get {
+                object obj = ResourceManager.GetObject("bg", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap exit {
             get {
                 object obj = ResourceManager.GetObject("exit", resourceCulture);
