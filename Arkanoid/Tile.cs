b81bde70b7363958855fc3071eafba67aff5ecc1
Cocknako <79690555+Cocknako@users.noreply.github.com>
2021-05-15 16:15:54 +0500

    Trying to update tiles, but this changed nothing

--- Arkanoid/Tile.cs
+++ Arkanoid/Tile.cs
@@ -1,9 +1,43 @@
+using System;
+
 namespace rndomNamespace
 {
     public class Tile
     {
         private const int Width = 50;
         private const int Height = 20;
+
+        private int Lenght;
+
+        private int[] Tiles;
+        
+
+        public Tile(int tileCount)
+        {
+            Lenght = tileCount;
+            Tiles = new int[tileCount];
+        }
+
+        public void Add(int tile)
+        {
+            
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index > Lenght)
+                    throw new IndexOutOfRangeException();
+                return Tiles[index];
+            }
+            set
+            {
+                if (index < 0 || index > Lenght)
+                    throw new IndexOutOfRangeException();
+                Tiles[index] = value;
+            }
+        }
 
         public int GetWidth => Width;
 
